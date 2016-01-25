d75056755f924da2f640a2850e423accdde60460
Abhishek <Abhishek291093@gmail.com>
2016-01-25 15:35:56 +0530

    login page is completed

--- front/app/scripts/main.js
+++ front/app/scripts/main.js
@@ -1,1 +1,22 @@
 console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+	$('.signInBox').hide('fast', function() {});
+	$('.helloButtonLogin').click(function(event) {
+		/* Act on the event */
+		$('.signInBox').show('fast', function() {
+			
+		});
+		$('.signUpBox').hide('fast', function() {
+			
+		});
+	});	
+	$('.helloButtonSignup').click(function(event) {
+		/* Act on the event */
+		$('.signUpBox').show('fast', function() {
+			
+		});
+		$('.signInBox').hide('fast', function() {
+			
+		});
+	});
+});

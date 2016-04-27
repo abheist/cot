bd4f1ce260e9f16a477d244e9654e9ebd6364cae
Kapil Agrawal <kapil.agrawal947@gmail.com>
2016-04-27 10:02:24 +0530

    Change view for anonymous questions

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -47,19 +47,14 @@
     </div>
     @endif
      @foreach($questions as $question)
+       @if(Auth::id()==$user->id)
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <a href="{{route('questions.show',$question->id)}}"><h3>{{ $question->question }}</h3></a>
-                        
-                        @if(!$question->anonymous)
                         <a  target="_blank" style="text-decoration: none;" class="btn-link" href={{ route('users.show',$question->user->id) }}> <img src="../../profile_default.png" class="img-circle" width="30" height="30">
                          <span class="label label-info">{{ $question->user->fname }} {{ $question->user->lname }} </span></a>
-                         @else
-                          <span class="label label-info">Anonymous </span>
-                          @endif
-
                           <br/>
                         <small> {{ date_format($question->created_at, 'g:i A \o\n l jS F Y') }} </small>
                     </div>
@@ -86,6 +81,41 @@
                 </div>
             </div>
         </div>
+        @elseif(!$question->anonymous)
+         <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <a href="{{route('questions.show',$question->id)}}"><h3>{{ $question->question }}</h3></a>
+                        <a  target="_blank" style="text-decoration: none;" class="btn-link" href={{ route('users.show',$question->user->id) }}> <img src="../../profile_default.png" class="img-circle" width="30" height="30">
+                         <span class="label label-info">{{ $question->user->fname }} {{ $question->user->lname }} </span></a>
+                          <br/>
+                        <small> {{ date_format($question->created_at, 'g:i A \o\n l jS F Y') }} </small>
+                    </div>
+                    <div class="panel-body">
+                        @if(count($question->answers))
+                            @if(count($question->answers)>1)
+                                {{ count($question->answers) }} Answers
+                            @else
+                                {{ count($question->answers) }} Answer
+                            @endif
+                            @foreach($question->answers as $answer)
+                                <div class="well">
+                                <a target="_blank" style="text-decoration: none;" class="btn-link" href={{ route('users.show',$question->user->id) }}> <img src="../../profile_default.png" class="img-circle" width="30" height="30">
+                             <span class="label label-info">{{ $answer->user->fname }} {{ $answer->user->lname }} </span></a> <br/>
+                                   
+                                    {{ $answer->answer }} 
+                                </div>
+                            @endforeach
+                          @else
+                            No Answers Available
+                        @endif
+                       <a href="{{ route('answers.create',$question->id) }}" class="btn btn-info">Answer</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endif
     @endforeach
 </div>
 @endsection
